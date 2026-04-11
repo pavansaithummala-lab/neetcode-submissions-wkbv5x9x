23233869de5bb1bcb0365df53c9482b8137aef9c
pavansaithummala-lab <pavanthemultinoblewinner@gmail.com>
2026-04-11 18:15:48 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> pre_product;
+        vector<int> post_product(nums.size());
+        vector<int> res;
+
+        int product=1;
+        for(int i=0; i<nums.size(); i++){
+            pre_product.push_back(product);
+            product = product*nums[i];
+        }
+        product=1;
+        for(int i=nums.size()-1; i>=0; i--){
+            post_product[i]=product;
+            product = product*nums[i];
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            res.push_back(post_product[i]*pre_product[i]);
+        }
+
+        return res;
+    }
+};
