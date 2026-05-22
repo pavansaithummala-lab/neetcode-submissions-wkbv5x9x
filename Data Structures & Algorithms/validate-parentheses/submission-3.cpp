910da4968a86f12c2c293f252c024e5a04d9c0a4
pavansaithummala-lab <pavanthemultinoblewinner@gmail.com>
2026-05-22 10:44:07 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for(char c : s){
+            if(c=='(' || c=='[' || c=='{'){
+                stk.push(c);
+            }
+
+            if(c==')'|| c ==']' || c=='}'){
+                if(stk.empty()){
+                    return false;
+                }
+                char top = stk.top();
+                if((c==')' && top !='(') || (c==']' && top !='[') || (c=='}' && top !='{')){
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+        if(stk.empty()){
+            return true;
+        }
+            return false;
+    }
+};
